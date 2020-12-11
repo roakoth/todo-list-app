012c17cb726a0927f3f955fee3d99c3450086029
roakoth <roseakoth2015@gmail.com>
2020-12-11 03:24:29 -0800

    added a picture

--- js/store.js
+++ js/store.js
@@ -83,13 +83,7 @@
 
     callback = callback || function () {};
 
-    // Generate an ID
-    // var newId = "";
-    // var charset = "0123456789";
-
-    // for (var i = 0; i < 6; i++) {
-    // 	newId += charset.charAt(Math.floor(Math.random() * charset.length));
-    // }
+    //Generate an ID
     var newId = Date.now();
 
     // If an ID was actually given, find the item and update each property
